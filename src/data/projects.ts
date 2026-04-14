c6ec1b2c059a91356dbba55287a078be279bd435
Nichole Nicolini <nchnicolini@gmail.com>
2026-04-13 23:40:11 -0300

    rotas entendidas

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,10 +16,19 @@ export const projects: Project[] = [
   {
     slug: 'deteccao-exoplanetas',
     mission: 'MISSAO #001',
-    title: 'Deteccao de exoplanetas',
+    title: 'Detecção de exoplanetas',
     stack: ['Python', 'Astropy', 'Pandas', 'ML'],
     description: 'Modelo de machine learnign para análise de curvas de luz para identificar transitos planetarios.',
-    status: 'concluida',
+    status: 'em-orbita',
+    moduleId: 'science',
+  },
+   {
+    slug: 'machine-learning-saude',
+    mission: 'MISSAO #002',
+    title: 'ML para saúde',
+    stack: ['Python', 'Astropy', 'Pandas', 'ML'],
+    description: 'Modelo de machine learnign para análise de curvas de luz para identificar transitos planetarios.',
+    status: 'em-orbita',
     moduleId: 'science',
   },
   // Adicione mais projetos aqui
